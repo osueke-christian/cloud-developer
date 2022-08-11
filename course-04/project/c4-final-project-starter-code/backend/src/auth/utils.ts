0cadc51b2296b304d82275c435a7fb06edc3bf39
Christian Osueke <osueke.christian@gmail.com>
2022-08-11 05:19:19 +0100

    feat: implement authentication using Auth0

--- course-04/project/c4-final-project-starter-code/backend/src/auth/utils.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/auth/utils.ts
@@ -11,3 +11,18 @@ export function parseUserId(jwtToken: string): string {
   const decodedJwt = decode(jwtToken) as JwtPayload
   return decodedJwt.sub
 }
+
+export function getToken(bearerToken): string {
+  if (!bearerToken) {
+    throw new Error('No authentication header');
+  }
+
+  if (!bearerToken.toLowerCase().startsWith('bearer ')) {
+    throw new Error('Invalid authentication header');
+  }
+
+  const split = bearerToken.split(' ');
+  const token = split[1];
+
+  return token;
+}
