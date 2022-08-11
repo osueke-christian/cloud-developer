6e73c1614103388cffb1b1bb2e94daee95911107
Christian Osueke <osueke.christian@gmail.com>
2022-08-11 05:21:58 +0100

    feat: implement todo crud and image upload

---
+++ course-04/project/c4-final-project-starter-code/backend/src/businessLogic/todoService.ts
@@ -0,0 +1,47 @@
+import * as uuid from 'uuid'
+
+import { TodoItem } from '../models/TodoItem'
+import { TodoUpdate } from '../models/TodoUpdate'
+import { TodoAccess } from '../dataLayer/todosAccess'
+import { CreateTodoRequest } from '../requests/CreateTodoRequest'
+import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
+
+const todoAccess = new TodoAccess()
+
+export async function getAllTodos(userId: string,): Promise<TodoItem[]> {
+  return todoAccess.getAllTodos(userId);
+}
+
+export async function createTodo(
+  userId: string,
+  createTodoRequest: CreateTodoRequest
+): Promise<TodoItem> {
+
+  const todoId = uuid.v4()
+  const s3BucketName = process.env.S3_BUCKET_NAME
+
+  return await todoAccess.createTodo({
+    userId,
+    todoId,
+    ...createTodoRequest,
+    attachmentUrl: `https://${s3BucketName}.s3.amazonaws.com/${todoId}`,
+    createdAt: new Date().toISOString(),
+    done: false
+  })
+}
+
+export async function updateTodo(
+  userId: string,
+  todoId: string,
+  updateTodoRequest: UpdateTodoRequest,
+): Promise<TodoUpdate> {
+  return todoAccess.updateTodo(userId, todoId, updateTodoRequest);
+}
+
+export function deleteTodo(userId: string, todoId: string): Promise<void> {
+  return todoAccess.deleteToDo(userId, todoId);
+}
+
+export function generateUploadUrl(todoId: string): Promise<string> {
+  return todoAccess.generateUploadUrl(todoId);
+}
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
@@ -4,15 +4,22 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { deleteTodo } from '../../businessLogic/todos'
+import { deleteTodo } from '../../businessLogic/todoService'
 import { getUserId } from '../utils'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const userId = getUserId(event);
     const todoId = event.pathParameters.todoId
-    // TODO: Remove a TODO item by id
+    const result = await deleteTodo(userId, todoId);
     
-    return undefined
+    return {
+      statusCode: 200,
+      headers: {
+          "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(result),
+    }
   }
 )
 
